05e3d3d25ea2860f8f33c72e60656cca5b677ad8
unknown <freddeman@STARFLEET.LOCAL>
2018-10-03 13:23:11 -0700

    added checks so there would no longer be index out of bounds errors when no input was given(just pressing enter)

--- Board.java
+++ Board.java
@@ -1,89 +1,89 @@
 import java.util.*;
 
 public class Board {
-   private char[] boardLetters;
-   private int length;
-   private String word;
-   private List<Character> usedLetters;
-   
-   public Board(String word) {
-      this.word = word;
-      usedLetters = new ArrayList<Character>();
-      length = word.length();
-      boardLetters = new char[length];
-      for(int i = 0; i < length; i++) {
-         char temp = word.charAt(i);
-         if(temp != ' ') {
-            boardLetters[i] = '_';      
-         }
-         else {
-            boardLetters[i] = ' ';
-         }
-      }
-   }
-   
-   public Board() {
-      this("");
-   }
-   
-   public String usedLetters() {
-      String letters = "";
-      if(usedLetters.size() > 0) {
-	 letters += usedLetters.get(0);
-      } 
-      for(int i = 1; i < usedLetters.size(); i++) {
-	 letters += "," + usedLetters.get(i);
-      }
-      return letters;
-   }
+	private char[]	boardLetters;
+	private int	length;
+	private String	word;
+	private List<Character>	usedLetters;
+	
+	public Board(String word) {
+		this.word =	word;
+		usedLetters	= new	ArrayList<Character>();
+		length =	word.length();
+		boardLetters =	new char[length];
+		for(int i =	0;	i < length;	i++) {
+			char temp =	word.charAt(i);
+			if(temp != ' ') {
+				boardLetters[i] =	'_';		 
+			}
+			else {
+				boardLetters[i] =	' ';
+			}
+		}
+	}
+	
+	public Board()	{
+		this("");
+	}
+	
+	public String usedLetters() {
+		String letters	= "";
+		if(usedLetters.size() >	0)	{
+	 letters	+=	usedLetters.get(0);
+		} 
+		for(int i =	1;	i < usedLetters.size();	i++) {
+	 letters	+=	"," +	usedLetters.get(i);
+		}
+		return letters;
+	}
 
-   public void addLetter(String s) {
-      if(contains(s)) {
+	public void	addLetter(String s) {
+		if(contains(s)) {
 	 set(s);
-       }
-       usedLetters.add(0, Character.toLowerCase(s.trim().charAt(0)));
-   }
+		 }
+		 usedLetters.add(0, Character.toLowerCase(s.trim().charAt(0)));
+	}
   
-   public boolean letterGuessed(String s) {
-      return usedLetters.contains(s.toLowerCase().trim().charAt(0));
-   }
+	public boolean	letterGuessed(String	s)	{
+		return usedLetters.contains(s.toLowerCase().trim().charAt(0));
+	}
 
-   public void set(String s) {
-      s = s.trim();
-      char c = s.charAt(0);
-      c = Character.toLowerCase(c);
-      for(int i = 0; i < length; i++) {
-         if(c == Character.toLowerCase(word.charAt(i))) {
-            boardLetters[i] = word.charAt(i);
-         }
-      }
-   }
-   
-   public boolean won() {
-      String workingWord = "";
-      for(int i = 0; i < length; i++) {
-    	 workingWord += boardLetters[i];
-      }
-      return word.equalsIgnoreCase(workingWord);
-   }
-   
-   public boolean contains(String s) { 
-      s = s.trim();
-      char c = s.charAt(0);
-      c = Character.toLowerCase(c);
-      for(int i = 0; i < length; i++) {
-         if(c == Character.toLowerCase(word.charAt(i))) {
-            return true;
-         }
-      }
-      return false;
-   }
-   
-   public String toString() {
-      String s = "";
-      for(int i = 0; i < length; i++) {
-         s += boardLetters[i] + " ";
-      }
-      return s;
-   }
+	public void	set(String s) {
+		s = s.trim();
+		char c =	s.charAt(0);
+		c = Character.toLowerCase(c);
+		for(int i =	0;	i < length;	i++) {
+			if(c == Character.toLowerCase(word.charAt(i))) {
+				boardLetters[i] =	word.charAt(i);
+			}
+		}
+	}
+	
+	public boolean	won()	{
+		String workingWord =	"";
+		for(int i =	0;	i < length;	i++) {
+		 workingWord += boardLetters[i];
+		}
+		return word.equalsIgnoreCase(workingWord);
+	}
+	
+	public boolean	contains(String s) {	
+		s = s.trim();
+		char c =	s.charAt(0);
+		c = Character.toLowerCase(c);
+		for(int i =	0;	i < length;	i++) {
+			if(c == Character.toLowerCase(word.charAt(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String toString() {
+		String s	= "";
+		for(int i =	0;	i < length;	i++) {
+			s += boardLetters[i]	+ " ";
+		}
+		return s;
+	}
 }
